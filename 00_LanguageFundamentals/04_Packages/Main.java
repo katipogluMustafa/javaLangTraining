4586981066397e04227ce1cb3972db5e419861b8
Mustafa <mustafakatipoglu98@hotmail.com>
2019-02-26 04:29:08 +0300

    Package constructing

---
+++ 00_LanguageFundamentals/04_Packages/Main.java
@@ -0,0 +1,10 @@
+import com.meshale.corejava.*;
+
+public class Main {
+    public static void main(String[] args){
+
+        Employee x = new Employee();
+        System.out.println(x.toString());
+
+    }
+}
