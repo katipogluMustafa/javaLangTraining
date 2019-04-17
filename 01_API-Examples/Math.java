8c90d3b7259240f378fa136f249bd7bfce4125f3
Yukawa <mustafakatipoglu98@hotmail.com>
2019-04-17 14:03:39 +0300

    Java.lang.Math API Examples

---
+++ 01_API-Examples/Math.java
@@ -0,0 +1,27 @@
+
+import static java.lang.Math.*;         // Static Import
+
+public class Test {
+    public static void main(String... args){
+
+        System.out.println("Math.max(5,4)\t\t = \t\t" + max(5,4));
+        System.out.println("Math.min(5,4)\t\t = \t\t" + min(5,4));
+        System.out.println("Math.pow(2,6)\t\t = \t\t" + pow(2,6));
+        System.out.println("Math.sqrt(81)\t\t = \t\t" + sqrt(81));
+
+        System.out.println("Math.abs(-5)\t\t = \t\t" + abs(-5));
+        System.out.println("Math.abs(0.0)\t\t = \t\t" + abs(0.0));
+        System.out.println("Math.ceil(-5.6)\t\t = \t\t" + ceil(-5.6));
+        System.out.println("Math.ceil(5.6)\t\t = \t\t" + ceil(5.6));
+        System.out.println("Math.floor(-5.6)\t = \t\t" + floor(-5.6));
+        System.out.println("Math.floor(5.6)\t\t = \t\t" + floor(5.6));
+        System.out.println("Math.cos(90)\t\t = \t\t" + cos(90));
+        System.out.println("Math.sin(0)\t\t\t = \t\t" + sin(0));
+        System.out.println("Math.tan(45)\t\t = \t\t" + tan(45));
+        System.out.println("Math.exp(5)\t\t\t = \t\t" + exp(5));
+        System.out.println("Math.log(Math.E)\t = \t\t" + log(Math.E));
+        System.out.println("Math.log10(Math.E)\t = \t\t" + log10(Math.E));
+
+
+    }
+}
